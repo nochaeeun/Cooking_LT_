ea50d269b1ff6afca5258d818601df7304b50444
jdjd11 <144589026+jdjd11@users.noreply.github.com>
2024-01-03 20:20:18 +0900

    손님등장/시간표시/메뉴주문
    개허접함

--- Assets/Script/Random_Menu.cs
+++ Assets/Script/Random_Menu.cs
@@ -6,15 +6,48 @@ using UnityEngine.UI;
 public class Random_Menu : MonoBehaviour
 {
     public Sprite Coffee;
+    public Sprite Bread;
+    public Sprite IceCream;
+    public Sprite Puding;
 
-    void Start()
+    public float aTIme;
+   // bool arr;
+
+    
+    void Awake()
     {
-        
+       // arr = Custmer.arrive;
+
+        int randomNumber = Random.Range(0, 4);
+        if (randomNumber == 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = Coffee;
+        }
+        else if (randomNumber == 1) 
+        {
+            GetComponent<SpriteRenderer>().sprite = Bread;
+        }
+        else if (randomNumber == 2) 
+        {
+            GetComponent<SpriteRenderer>().sprite = IceCream;
+
+        }
+        else 
+        {
+            GetComponent<SpriteRenderer>().sprite = Puding;
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        aTIme = Custmer.arrivalTime;
+        if (aTIme <= 0.5f) 
+        {
+            Destroy(gameObject);
+        }
+        Debug.Log(aTIme);
     }
 }
